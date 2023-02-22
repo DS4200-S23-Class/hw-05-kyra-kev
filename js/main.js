e6507bce109c8c665c6d72fb98e4e8c981beb154
kqueally <queally.k@northeastern.edu>
2023-02-21 21:16:41 -0500

    almost done

--- js/main.js
+++ js/main.js
@@ -3,12 +3,35 @@ const FRAME_HEIGHT = 400;
 const FRAME_WIDTH = 400;
 const MARGINS = {left: 40, right: 40, top: 40, bottom: 40};
 
-
+// adding
 const FRAME1 = d3.select("#visGraph1")
                   .append("svg")
                     .attr("height", FRAME_HEIGHT)
                     .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame");
+                    .attr("id", "frame");
+
+
+d3.csv("data/scatter-data.csv").then((data) => {
+  FRAME1.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("cx", (d) => {
+          return d.x * 40;
+        })
+        .attr("cy", (d) => { 
+          return 400 - (d.y * 40);
+        })
+        .attr("r", 10)
+        .attr("class", "point")
+        .attr("id", (d) => {
+          return d.x.toString() + d.y.toString();
+        })
+        .attr("onclick", (d) => {
+          return "addBorder(" + d.x.toString() + d.y.toString() + ")";
+        });
+
+});
 
 // functions for point functionality
 
@@ -16,11 +39,11 @@ const FRAME1 = d3.select("#visGraph1")
 // displayed as the last point clicked
 function addBorder(id) {
   // toggle border
-  let point = document.getElementById(id);
+  let point = d3.select("#" + id);
   point.classList.toggle("border");
 
   // alter text
-  let p = document.getElementById("coord-id");
+  let p = d3.select(".coord-id");
   p.innerHTML = "Last point clicked: " + "("
     + id.toString().charAt(0) + "," + id.toString().charAt(1) +")";
 }
@@ -28,8 +51,8 @@ function addBorder(id) {
 // adds a point with the given coordinates from the dropdown
 function addPoint() {
     // get the coords
-    let x = document.getElementById("x-coord").value;
-    let y = document.getElementById("y-coord").value;
+    let x = d3.select(".x-coord").value;
+    let y = d3.select(".y-coord").value;
     let id = x.toString() + y.toString();
 
     FRAME1.append("circle")
@@ -42,12 +65,4 @@ function addPoint() {
 }
 
 // adds event listener to submit button
-document.getElementById("submit").addEventListener("click", addPoint);
-
-// adds event listeners to all points
-let points = document.getElementsByClassName("point");
-for (let i = 0; i < points.length; i++) {
-     points[i].addEventListener("click", () => {
-        addBorder(points[i].id);
-     })
-    };
+d3.select(".submit").addEventListener("click", addPoint);
